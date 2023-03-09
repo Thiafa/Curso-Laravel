9cc25ead31fb3d699c4dc3df93868598fd9d725d
Thiafa <thiafa.vos@gmail.com>
2023-03-09 15:43:57 -0300

    Encaminhamento de parâmetros para o controlador

--- app_super_gestao/resources/views/site/sobre-nos.blade.php
+++ app_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -1,7 +1,7 @@
 <h3>Sobre</h3>
 
 <ul> 
-  <li><a href="/">Home</a></li>
-  <li><a href="sobre">Sobre Nós</a></li>
-  <li><a href="contato">Contato</a></li>
+  <li><a href="{{route('site.index')}}">Home</a></li>
+  <li><a href="{{route('site.sobrenos')}}">Sobre Nós</a></li>
+  <li><a href="{{route('site.contato')}}">Contato</a></li>
 </ul>
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\SobreController;
+use App\Http\Controllers\TesteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,26 +17,32 @@ use App\Http\Controllers\SobreController;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'mostrarPrincipal']);
+Route::get('/', [PrincipalController::class, 'mostrarPrincipal'])->name('site.index');
 
-Route::get('/sobre', [SobreController::class, 'mostrarSobre']);
+Route::get('/sobre', [SobreController::class, 'mostrarSobre'])->name('site.sobrenos');
 
-Route::get('/contato', [ContatoController::class,'mostrarContato']);
+Route::get('/contato', [ContatoController::class,'mostrarContato'])->name('site.contato');
 
 Route::get('/login', function(){
     return 'teste';
+})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){
+        return 'teste';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function(){
+        return 'teste';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function(){
+        return 'teste';
+    })->name('app.produtos');
 });
 
-Route::get('/clientes', function(){
-    return 'teste';
-});
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('site.rota1'); 
 
-Route::get('/fornecedores', function(){
-    return 'teste';
-});
-
-Route::get('/produtos', function(){
-    return 'teste';
+Route::fallback(function(){
+   echo 'A rota acessada não existe. <a href="'.route('site.index').'">home</a>';
 });
 
 // Route::get(
@@ -44,3 +51,15 @@ Route::get('/produtos', function(){
 //         echo "Olá $nome $categoria_id";
 //     }
 // )->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
+
+
+// Route::get('/rota1', function(){
+// echo 'Rota 1';
+// })->name('site.rota1'); 
+
+// Route::get('/rota2', function(){
+//     return redirect()->route('site.rota1');
+// })->name('site.rota2'); 
+
+// Route::redirect('/rota2', 'rota1');
+
