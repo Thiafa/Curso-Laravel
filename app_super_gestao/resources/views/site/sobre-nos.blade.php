cedbac16a20c08dcca715730c530e187ef1a723d
Thiafa <thiafa.vos@gmail.com>
2023-03-09 14:51:33 -0300

    Criando estrutura base e menu navegação

---
+++ app_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,7 @@
+<h3>Sobre</h3>
+
+<ul> 
+  <li><a href="/">Home</a></li>
+  <li><a href="sobre">Sobre Nós</a></li>
+  <li><a href="contato">Contato</a></li>
+</ul>
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,21 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    echo "Rotas em Laravel";
-    echo "</br><a href='contato'>Contato</a>";
-    echo "<br><a href='sobre'>Sobre</a>";
-});
-Route::get('/contato', function () {
-    echo "Contato";
-});
-Route::get('/contato/{telefone}', function (int $telefone) {
-    echo "Seu telefone é: $telefone";
-});
-Route::get('/sobre', function () {
-    echo "Sobre";
-});
-Route::get('/perfil/{usuario}/{id}', function ($usuario, int $id) {
-    echo "Seja bem vindo $usuario. Seu id do usuário é $id";
-})->where('id',"[0-9]+")->where('usuario','[A-Za-z]+');
+Route::get('/', [PrincipalController::class, 'mostrarPrincipal']);
 
+
+Route::get('/sobre', [SobreController::class, 'mostrarSobre']);
+
+Route::get('/contato', [ContatoController::class,'mostrarContato']);
+
+// Route::get(
+//     '/contato/{nome}/{categoria_id}',
+//     function (string $nome, int $categoria_id){
+//         echo "Olá $nome $categoria_id";
+//     }
+// )->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
